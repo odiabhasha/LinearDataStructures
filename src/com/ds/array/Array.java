1afadd13b9d72b48fc971d6fd18920ccdedd6708
priyabrat2k <priyabrat2k@gmail.com>
2020-08-29 13:22:20 +0530

    Delete At method Implemented.

--- src/com/ds/array/Array.java
+++ src/com/ds/array/Array.java
@@ -1,4 +1,34 @@
 package com.ds.array;
 
 public class Array {
+    private int [] array;
+    private int count;
+
+    public Array(int length){
+        array = new int[length];
+    }
+
+    public void insert(int item){
+        if(array.length == count){
+            int []newArray = new int[count * 2];
+            for (int i=0;i<count;i++)
+                newArray[i] = array[i];
+            array = newArray;
+        }
+        array[count++] = item;
+    }
+
+    public void deleteAt(int index){
+        if(index < 0 || index >= count)
+            throw new IllegalArgumentException();
+        for(int i = index ; i < count;i++)
+            array[i] = array[i+1];
+        count--;
+
+    }
+
+    public void print(){
+        for (int i=0; i<count; i++)
+            System.out.println(array[i]);
+    }
 }
--- src/com/ds/array/ArrayMain.java
+++ src/com/ds/array/ArrayMain.java
@@ -3,6 +3,11 @@ package com.ds.array;
 public class ArrayMain {
 
     public static void main(String[] args) {
-
+        Array array = new Array(3);
+        array.insert(12);
+        array.insert(13);
+        array.insert(14);
+        array.insert(15);
+        array.print();
     }
 }
