a1ee0923756b9fa3434f11371ea7ccdea09a8695
priyabrat2k <priyabrat2k@gmail.com>
2020-08-30 01:01:35 +0530

    Intersection will be implemented.

--- src/com/ds/array/Array.java
+++ src/com/ds/array/Array.java
@@ -18,12 +18,47 @@ public class Array {
         array[count++] = item;
     }
 
+    public void insertAt(int index, int item){
+
+    }
+
+    public int max(){
+        //if current item is greater than max, max will be equal to current item and iterate the whole array
+        int max = 0;
+        for(int i=0;i<count;i++)
+            if(array[i] > max)
+                max = array[i];
+        return max;
+    }
+
     public void deleteAt(int index){
         if(index < 0 || index >= count)
             throw new IllegalArgumentException();
         for(int i = index ; i < count;i++)
             array[i] = array[i+1];
         count--;
+    }
+
+    public int indexOf(int item){
+        for(int i=0;i<count;i++)
+            if(array[i] == item)
+                return i;
+        return -1;
+    }
+
+    public boolean contains(int item){
+        for(int i=0; i<count; i++)
+            if(array[i] == item)
+                return true;
+        return false;
+    }
+
+    public void reverse(){
+        int newArray[] = new int[count];
+        int index = 0;
+        for(int i=count-1;i>=0;i--)
+            newArray[index++] = array[i];
+        array = newArray;
 
     }
 
--- src/com/ds/array/ArrayMain.java
+++ src/com/ds/array/ArrayMain.java
@@ -8,6 +8,15 @@ public class ArrayMain {
         array.insert(13);
         array.insert(14);
         array.insert(15);
+        /*System.out.println(array.indexOf(12));
+        System.out.println(array.indexOf(13));
+        System.out.println(array.indexOf(15));
+        System.out.println(array.max());
+        array.deleteAt(0);*/
+        array.print();
+
+        array.reverse();
+
         array.print();
     }
 }
